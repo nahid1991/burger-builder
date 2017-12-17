c9cbcf1eac2ad19f4bceb2d2436d5f9f418943ba
Nahid Shaiket <nahidshaiket10300@gmail.com>
2017-12-17 18:07:32 +0600

    created the burger logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layouts/Layout';
+
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
